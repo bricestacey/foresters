bf198fc0fe6ea4e8e8f926368db6199f85c716c7
Brice Stacey <bricestacey@gmail.com>
2011-11-17 19:18:57 -0500

    bugfix

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'csv'
+ThinkingSphinx.deltas_suspended = true
 
 CSV.foreach(File.join(Rails.root, 'db', 'courts.csv'), headers: true) do |row|
   court = Court.create({id: row['id'], 
@@ -22,33 +23,35 @@ CSV.foreach(File.join(Rails.root, 'db', 'courts.csv'), headers: true) do |row|
   court.ethnicities << ethnicities unless ethnicities.nil?
 end
 
-Record.suspended_delta do
-  CSV.foreach(File.join(Rails.root, 'db', 'records.csv'), headers: true) do |row|
-    record = Record.create!({
-      claim_id: row['Death Claim Number'],
-      age_at_initiation: row['Age at Initiation'],
-      notes_private: "Missing: #{row['Missing']}"
-    }.tap do |attributes|
-      if row['Text Form Initiation date (for sorting)']
-        attributes[:initiated_on_year]  = row['Text Form Initiation date (for sorting)'][0..3]
-        attributes[:initiated_on_month] = row['Text Form Initiation date (for sorting)'][4..5]
-        attributes[:initiated_on_date]  = row['Text Form Initiation date (for sorting)'][6..7]
-      end
-      if row['Text Form Death Date (for sorting)']
-        attributes[:died_on_year]  = row['Text Form Death Date (for sorting)'][0..3]
-        attributes[:died_on_month] = row['Text Form Death Date (for sorting)'][4..5]
-        attributes[:died_on_date]  = row['Text Form Death Date (for sorting)'][6..7]
-      end
-    end)
+CSV.foreach(File.join(Rails.root, 'db', 'records.csv'), headers: true) do |row|
+puts row.inspect
+exit
+  puts row['Death Claim Number']
+  record = Record.create!({
+    claim_id: row['Death Claim Number'],
+    age_at_initiation: row['Age at Initiation'],
+    notes_private: "Missing: #{row['Missing']}"
+  }.tap do |attributes|
+    if row['Text Form Initiation date (for sorting)']
+      attributes[:initiated_on_year]  = row['Text Form Initiation date (for sorting)'][0..3]
+      attributes[:initiated_on_month] = row['Text Form Initiation date (for sorting)'][4..5]
+      attributes[:initiated_on_date]  = row['Text Form Initiation date (for sorting)'][6..7]
+    end
+    if row['Text Form Death Date (for sorting)']
+      attributes[:died_on_year]  = row['Text Form Death Date (for sorting)'][0..3]
+      attributes[:died_on_month] = row['Text Form Death Date (for sorting)'][4..5]
+      attributes[:died_on_date]  = row['Text Form Death Date (for sorting)'][6..7]
+    end
+  end)
 
-    record.names.create!({
-      last: row["Insured's Surname"],
-      first: row["Given Name"],
-      middle: row['Middle Name or Initial'],
-      title: row['Title'],
-    })
+  record.create_residence
+  record.names.create!({
+    last: row["Insured's Surname"],
+    first: row["Given Name"],
+    middle: row['Middle Name or Initial'],
+    title: row['Title'],
+  })
 
-    record.court = Court.find_by_code(row['Court #'])
-    raise if record.court.nil?
-  end
+  record.court = Court.find_by_code(row['Court # '])
+  raise if record.court.nil?
 end
